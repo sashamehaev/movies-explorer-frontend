ab0b2cc61eb0978d318701bcef831ce71cd884c6
sashamehaev <alexandrmekhaev@yandex.ru>
2023-02-19 19:36:44 +0300

    test

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,9 @@ import './NotFound.css';
 function NotFound() {
     return (
         <>
-            <h2 className="not-found__header">404</h2>
+            <h2 className="not-found__header">4044044444444</h2>
+            <h2 className="not-found__header">4044044444444</h2>
+            <h2 className="not-found__header">4044044444444</h2>
             <p className="not-found__text">Страница не найдена</p>
             <p className="not-found__link">Назад</p>
 
